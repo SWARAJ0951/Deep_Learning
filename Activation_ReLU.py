3e6bad30eddac0abe0fc5f504193b480678fb4e4
Swaraj Jagtap <swarajjagtap22@gmail.com>
2026-08-31 22:53:50 +0530

    Add files via upload

---
+++ Activation_ReLU.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def ReLU(z):
+    return max(0,z)
+
+def Marvellous_Neuron_Forward(inputs,weights,bias):
+    print("Inputs are (X) :",inputs)
+    print("Weights are (W):",weights)
+    print("bias is (b):",bias)
+
+    z = 0
+
+    for i in range(len(inputs)):
+        z = z + (inputs[i] * weights[i])
+    z = z + bias 
+
+    #z = sum(w*x for w,x in zip(weights,inputs))+bias
+    print("Weighted Sum (Z):",z)
+
+    y = ReLU(z)
+
+    return y
+
+def main():
+    print("-------Marvellous Neural Network--------")
+
+    inputs = [1.0,2.0,3.0]
+    weights = [0.6,0.4,-0.2]
+    bias = 0.5
+
+    result = Marvellous_Neuron_Forward(inputs,weights,bias)
+
+    print(result)
+
+if __name__ =="__main__":
+    main()
